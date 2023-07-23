e965b2932c27f2c3dc9517142a0c1217888b78cd
Bouzidi Mohamed Ali <bouzidi.medali20@gmail.com>
2023-07-23 20:22:37 +0200

    init project

---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("simple_shell\n");
+    return 0;
+}
---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H_
+#define SHELL_H_
+
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+extern char **environ;
+
+#endif
